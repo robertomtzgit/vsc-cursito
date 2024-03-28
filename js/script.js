383af937bca9860ce9251d45240d17b15fb6c4f4
Roberto Martinez <robertocarlosmartinez.contact@gmail.com>
2024-03-28 10:44:37 -0600

    prueba de codigo-2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-numA = 30
-numB = 20
+// Funcion para sumar dos numeros
+function sumar(a, b) {
+    return a + b; //Error intencional: deberia ser a + b
+}
 
-res = numB + numA
-console.log(res)
+// Usando la función para sumar dos números
+let numero1 = 5;
+let numero2 = 2;
+let numero3 = 6;
+
+// Lamada a la función y almacenamiento del resultado
+let resultado = sumar(numero1, numero2);
+
+// Mostrar resultado
+console.log("El resultado de sumar " + numero1 + " y " + numero2 + " es: " + resultado)
